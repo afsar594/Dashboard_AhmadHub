f60f5267f2d7458469a2d783ebabb081252efd8f
AfsarRiaz <malikafsar594@gmail.com>
2026-01-25 07:25:53 +0500

    m

--- src/app/component/admin-login/admin-login.component.ts
+++ src/app/component/admin-login/admin-login.component.ts
@@ -1,28 +1,34 @@
 import { Component } from '@angular/core';
 import {
   FormBuilder,
+  FormGroup,
   FormsModule,
   ReactiveFormsModule,
   Validators,
 } from '@angular/forms';
 import { Router } from '@angular/router';
 import { ApiService } from '../../service/api.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-admin-login',
   standalone: true,
-  imports: [FormsModule, ReactiveFormsModule],
+  imports: [CommonModule, FormsModule, ReactiveFormsModule],
   templateUrl: './admin-login.component.html',
   styleUrl: './admin-login.component.css',
 })
 export class AdminLoginComponent {
-  loginForm: any;
+  loginForm!: FormGroup;
+  submitted = false;
+  showPassword = false; // ✅ track show/hide password
 
   constructor(
-    private router: Router,
-    private api: ApiService,
     private fb: FormBuilder,
+    private api: ApiService,
+    private router: Router,
   ) {
+    // Validators.email
+    // , Validators.minLength(5), Validators.maxLength(8)
     this.loginForm = this.fb.group({
       email: ['', [Validators.required]],
       password: ['', [Validators.required]],
@@ -30,18 +36,36 @@ export class AdminLoginComponent {
   }
 
   onSubmit() {
+    this.submitted = true;
+    console.log('Form Values:', this.loginForm.value);
+
     if (this.loginForm.valid) {
-      console.log('Form Values:', this.loginForm.value);
-      this.api.login('').subscribe((r) => {
-        alert('Login successful!');
+      let body = this.loginForm.value;
+      let payload = {
+        id: 0,
+        username: body.email,
+        email: body.email,
+        pwd: body.password,
+        role: 'Admin',
+      };
+
+      this.api.login(payload).subscribe((res: any) => {
+        if (res.isSuccess) {
+          this.router.navigate(['dashboard']);
+        } else {
+          alert(res.message);
+        }
       });
     }
   }
 
   hasError(controlName: string, errorName: string) {
-    return this.loginForm.get(controlName)?.hasError(errorName);
+    return (
+      this.submitted && this.loginForm.get(controlName)?.hasError(errorName)
+    );
   }
-  navigatetodashboard() {
-    this.router.navigate(['dashboard']);
+
+  togglePassword() {
+    this.showPassword = !this.showPassword;
   }
 }
