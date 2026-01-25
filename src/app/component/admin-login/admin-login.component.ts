3247549067b2d81a824c94dc0a448ab712a061a5
AfsarRiaz <malikafsar594@gmail.com>
2026-01-25 06:57:47 +0500

    update

--- src/app/component/admin-login/admin-login.component.ts
+++ src/app/component/admin-login/admin-login.component.ts
@@ -1,27 +1,45 @@
 import { Component } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import {
+  FormBuilder,
+  FormsModule,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 import { Router } from '@angular/router';
+import { ApiService } from '../../service/api.service';
 
 @Component({
   selector: 'app-admin-login',
   standalone: true,
-  imports: [FormsModule],
+  imports: [FormsModule, ReactiveFormsModule],
   templateUrl: './admin-login.component.html',
   styleUrl: './admin-login.component.css',
 })
 export class AdminLoginComponent {
-  username = '';
-  password = '';
+  loginForm: any;
 
-  constructor(private router: Router) {}
+  constructor(
+    private router: Router,
+    private api: ApiService,
+    private fb: FormBuilder,
+  ) {
+    this.loginForm = this.fb.group({
+      email: ['', [Validators.required]],
+      password: ['', [Validators.required]],
+    });
+  }
 
-  login() {
-    if (this.username === 'admin' && this.password === '12345') {
-      localStorage.setItem('admin', 'true');
-      this.router.navigate(['/admin/dashboard']);
-    } else {
-      alert('Invalid login');
+  onSubmit() {
+    if (this.loginForm.valid) {
+      console.log('Form Values:', this.loginForm.value);
+      this.api.login('').subscribe((r) => {
+        alert('Login successful!');
+      });
     }
+  }
+
+  hasError(controlName: string, errorName: string) {
+    return this.loginForm.get(controlName)?.hasError(errorName);
   }
   navigatetodashboard() {
     this.router.navigate(['dashboard']);
